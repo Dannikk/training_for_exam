627b8d3a91d40c82707a85c00d210c718c476c54
Dannikk <nikita.danilov0103@gmail.com>
2021-03-23 14:10:35 +0300

    Redundant imports deleted

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-from numpy import *
 
 
 class DirReader:
@@ -16,45 +15,62 @@ class DirReader:
     def __enter__(self):
         return self.generator()
 
-
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("exiting")
+        # print("exiting")
         return 0
-
-    # def __iter__(self):
-    #     return self.generator()
 
 
 def graph_builder(files: str):
     G = {}
     for file_path in files:
-        print(file_path)
+        # print(file_path)
         with open(file_path, "r") as file:
-            file.readline()
+            _ = file.readline()
             for row in file:
                 names = list(set(get_names(row)))
                 G = graph_append(G, names)
+    return G
 
 
 def graph_append(graph: dict, names: list):
     for name in names:
         if name not in graph:
-            # как было
-            graph[name] = names
-            # как буд делать
-            # graph[name] = names
+            graph[name] = names.copy()
         else:
-            # костыль, которым решалась проблема
-            # a = graph[name].copy()
-            # a.extend(names)
-            # graph[name] = a
-            print(type(graph[name]))
-            graph[name].extend(names) # а чо не работает?
+            graph[name].extend(names)  # а чо не работает?
     return graph
 
 
-def graph_iterator():
-    ...
+def iter_decorator(g_iter=None, *, output="res_1.txt"):
+    print("iter_decorator:")
+    if g_iter is None:
+        print("g_iter is None:")
+        return lambda g_iter: iter_decorator(g_iter, output=output)
+    print("g_iter is not None:")
+    def wrapper(graph):
+        print("wrapper:")
+        out_fl = open(output, "w")
+        for i, j in g_iter(graph):
+            print(i, j, file=out_fl)
+            yield i, j
+        out_fl.close()
+    return wrapper
+
+
+@iter_decorator(output="resss_2.txt")
+class GraphIterator:
+    def __init__(self, g: dict):
+        self.graph = g
+        self.weights = [(scientists.count(name), name) for name, scientists in self.graph.items()]
+
+    def __iter__(self):
+        return self.generator()
+
+    def generator(self):
+        self.weights.sort(reverse=True)
+        for name, count in self.weights:
+            yield name, count
+
 
 def get_names(research):
     names = research.split('\t')[:-1]
@@ -63,7 +79,61 @@ def get_names(research):
 
 path_name = "../data"
 with DirReader(path_name) as files:
-    print(type(files))
     # for file in files:
     #     print(file)
-    graph_builder(files)
+    graph = graph_builder(files)
+    # output = open("result.txt", "w")
+    file = "results.txt"
+    git = GraphIterator(graph)
+    for i, j in git:
+        print("Result: ", i, j)
+    # output.close()
+
+# def decor(func):
+#     def wrapper(g_iter):
+#         print("wrapper")
+#         f = func(g_iter)
+#         print(f)
+#         return f
+#     return wrapper
+#
+#
+# class Tmp:
+#     def __init__(self, x, y):
+#         self.x, self.y = x, y
+#
+#     @decor
+#     def function(self):
+#         print("func")
+#         return self.x*self.y + self.x + self.y
+#
+# tmp = Tmp(1, 2)
+# print(tmp.function())
+
+
+# def decor(g_iter):
+#     def wrapper(*args):
+#         print("wrapper:")
+#         for i in args:
+#             print(type(i))
+#         # f = func(g_iter)
+#         # print(f)
+#         return args[0]
+#
+#     return wrapper
+#
+#
+# @decor
+# class Tmp:
+#     def __init__(self, x, y):
+#         self.x, self.y = x, y
+#
+#     def function(self):
+#         print("func:")
+#         return self.x * self.y + self.x + self.y
+#
+#
+# tmp = Tmp(1, 2)
+# print("print:", type(tmp))
+
+
